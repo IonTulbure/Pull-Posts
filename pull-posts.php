e7036c060bb33915ed44d5c414fd818a61abe598
Ion <42011629+IonTulbure@users.noreply.github.com>
2024-01-18 23:47:47 +0200

    Added plugin bootstrap file (#1)

---
+++ pull-posts.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://mustcode.xyz
+ * @since             1.0.0
+ * @package           Pull_Posts
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Pull Posts
+ * Plugin URI:        https://mustcode.xyz
+ * Description:       Simple Wordpress Plugin to Pull posts from remote WP site
+ * Version:           1.0.0
+ * Author:            Ion Tulbure
+ * Author URI:        https://mustcode.xyz/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       pull-posts
+ * Domain Path:       /languages
+ */
